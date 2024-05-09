86dbb50d19c8f760aeb72e9589d119305ffba864
Kazeru <thanhle8461@gmail.com>
2024-05-09 21:04:19 +0700

    New Login Screen

--- src/swing/Button.java
+++ src/swing/Button.java
@@ -1,46 +1,88 @@
 package swing;
 
+import java.awt.AlphaComposite;
 import java.awt.Color;
 import java.awt.Cursor;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Point;
 import java.awt.RenderingHints;
-import java.awt.Shape;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.image.BufferedImage;
 import javax.swing.JButton;
 import javax.swing.border.EmptyBorder;
-import raven.fbr.FancyBorderRadius;
+import org.jdesktop.animation.timing.Animator;
+import org.jdesktop.animation.timing.TimingTarget;
+import org.jdesktop.animation.timing.TimingTargetAdapter;
 
-/**
- *
- * @author RAVEN
- */
 public class Button extends JButton {
 
-    private Shape shape;
-    private final RippleEffect rippleEffect;
+    public Color getEffectColor() {
+        return effectColor;
+    }
+
+    public void setEffectColor(Color effectColor) {
+        this.effectColor = effectColor;
+    }
+
+    private Animator animator;
+    private int targetSize;
+    private float animatSize;
+    private Point pressedPoint;
+    private float alpha;
+    private Color effectColor = new Color(255, 255, 255);
 
     public Button() {
-        rippleEffect = new RippleEffect(this);
         setContentAreaFilled(false);
-        setBorder(new EmptyBorder(8, 5, 8, 5));
+        setBorder(new EmptyBorder(5, 0, 5, 0));
+        setBackground(Color.WHITE);
         setCursor(new Cursor(Cursor.HAND_CURSOR));
+        addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent me) {
+                targetSize = Math.max(getWidth(), getHeight()) * 2;
+                animatSize = 0;
+                pressedPoint = me.getPoint();
+                alpha = 0.5f;
+                if (animator.isRunning()) {
+                    animator.stop();
+                }
+                animator.start();
+            }
+        });
+        TimingTarget target = new TimingTargetAdapter() {
+            @Override
+            public void timingEvent(float fraction) {
+                if (fraction > 0.5f) {
+                    alpha = 1 - fraction;
+                }
+                animatSize = fraction * targetSize;
+                repaint();
+            }
+        };
+        animator = new Animator(700, target);
+        animator.setAcceleration(0.5f);
+        animator.setDeceleration(0.5f);
+        animator.setResolution(0);
     }
 
     @Override
     protected void paintComponent(Graphics grphcs) {
-        Graphics2D g2 = (Graphics2D) grphcs.create();
+        int width = getWidth();
+        int height = getHeight();
+        BufferedImage img = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+        Graphics2D g2 = img.createGraphics();
         g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-        g2.setColor(new Color(255, 255, 255, 80));
-        g2.fill(shape);
-        rippleEffect.reder(g2, shape);
+        g2.setColor(getBackground());
+        g2.fillRoundRect(0, 0, width, height, height, height);
+        if (pressedPoint != null) {
+            g2.setColor(effectColor);
+            g2.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_ATOP, alpha));
+            g2.fillOval((int) (pressedPoint.x - animatSize / 2), (int) (pressedPoint.y - animatSize / 2), (int) animatSize, (int) animatSize);
+        }
         g2.dispose();
+        grphcs.drawImage(img, 0, 0, null);
         super.paintComponent(grphcs);
     }
-
-    @Override
-    public void setBounds(int i, int i1, int i2, int i3) {
-        super.setBounds(i, i1, i2, i3);
-        shape = new FancyBorderRadius(getWidth(), getHeight(), "100% 0% 100% 0% / 0% 100% 0% 100% ").getShape();
-    }
-
 }
